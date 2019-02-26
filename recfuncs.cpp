1292c56cd34770af5169fcb97944ec5d611143f3
Jan Kaufmann <jan.kaufmann@liwest.at>
2019-02-26 11:21:51 +0100

    kinda builds

---
+++ recfuncs.cpp
@@ -0,0 +1,32 @@
+#include "recfuncs.h"
+
+int calc_array_size(int n)
+{
+  return 0;
+}
+
+int convert_to_binary(int n)
+{
+  return 0;
+}
+
+int* create_array(int size)
+{
+  int name[size];
+}
+
+int calc_factorial(int n)
+{
+  return 0;
+}
+
+void calc_sums(int* nums, int size, int* out_arr, int* out_arr_bin)
+{
+
+}
+
+CalculationResults perform_calculations(int* nums, int n)
+{
+  struct CalculationResults temp;
+  return temp;
+}
