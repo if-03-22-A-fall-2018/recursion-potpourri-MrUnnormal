1d59ae51f28c5b009190e4c16b2a27bd1385e2f3
Jan Kaufmann <jan.kaufmann@liwest.at>
2019-03-05 15:17:27 +0100

    builds / finished

--- recfuncs.cpp
+++ recfuncs.cpp
@@ -1,32 +1,90 @@
+#include <stdlib.h>
 #include "recfuncs.h"
 
 int calc_array_size(int n)
 {
-  return 0;
+  if(n <= 0)
+  {
+    return 1;
+  }
+  return 2 * calc_array_size(n - 1);
 }
 
 int convert_to_binary(int n)
 {
+  if(n != 0)
+  {
+    return (n % 2) + (10 * (convert_to_binary(n / 2)));
+  }
   return 0;
 }
 
 int* create_array(int size)
 {
-  int name[size];
+  if(size > 0)
+  {
+    int new_array_size = calc_array_size(size);
+    int* new_array = (int*) malloc(sizeof(int) * new_array_size);
+
+    for(int i = 0; i < new_array_size; i++)
+    {
+      new_array[i] = -1;
+    }
+
+    return new_array;
+  }
+  else
+  {
+    return 0;
+  }
 }
 
 int calc_factorial(int n)
 {
-  return 0;
+  if(n <= 1)
+  {
+    return 1;
+  }
+  else
+  {
+    return n * calc_factorial(n - 1);
+  }
 }
 
 void calc_sums(int* nums, int size, int* out_arr, int* out_arr_bin)
 {
-
+  do_subset(nums, size - 1, 0, 0, out_arr, out_arr_bin, 0);
 }
 
-CalculationResults perform_calculations(int* nums, int n)
+int do_subset(int* nums, int size, int i, int index, int* out_arr, int* out_arr_bin, int sum)
 {
-  struct CalculationResults temp;
-  return temp;
+
+  if(i > size)
+  {
+    out_arr[index] = sum;
+    out_arr_bin[index] = convert_to_binary(sum);
+    return ++index;
+  }
+
+  int index1 = do_subset(nums, size, i + 1, index, out_arr, out_arr_bin, sum + nums[i]);
+  do_subset(nums, size, i + 1, index1, out_arr, out_arr_bin, sum);
+}
+
+struct CalculationResults perform_calculations(int* arr, int num)
+{
+  struct CalculationResults calc_results;
+  calc_results.num_factorials = create_array(num);
+  calc_results.no_of_nums = num;
+  calc_results.no_of_sums = calc_array_size(num);
+
+  calc_results.binary_sums = create_array(calc_results.no_of_sums);
+  calc_results.sums = create_array(calc_results.no_of_sums);
+
+  for(int i = 0; i < num; i++)
+  {
+    calc_results.num_factorials[i] = calc_factorial(arr[i]);
+  }
+
+  calc_sums(arr, num, calc_results.sums, calc_results.binary_sums);
+  return calc_results;
 }
--- recfuncs.h
+++ recfuncs.h
@@ -1,5 +1,6 @@
 #ifndef ___RECURSIVE_POTPOURRI
 #define ___RECURSIVE_POTPOURRI
+
 struct CalculationResults {
   int no_of_nums;
   int* sums;
@@ -13,6 +14,7 @@ int convert_to_binary(int n);
 int* create_array(int size);
 int calc_factorial(int n);
 void calc_sums(int* nums, int size, int* out_arr, int* out_arr_bin);
+int do_subset(int* nums, int size, int i, int index, int* out_arr, int* out_arr_bin, int sum);
 CalculationResults perform_calculations(int* nums, int n);
 
 #endif
